bd8c08b29d560b316be4fc129a44ab9bb005fef5
Prasanna <prasannacse2005@gmail.com>
2021-01-06 15:54:34 +0530

    add domain models

---
+++ src/com/routebee/domains/ParkingLot.java
@@ -0,0 +1,15 @@
+package com.routebee.domains;
+
+import java.util.List;
+
+public class ParkingLot {
+    private List<Slot> slots;
+
+    public List<Slot> getSlots() {
+        return slots;
+    }
+
+    public void setSlots(List<Slot> slots) {
+        this.slots = slots;
+    }
+}
---
+++ src/com/routebee/domains/Slot.java
@@ -0,0 +1,13 @@
+package com.routebee.domains;
+
+public class Slot {
+    private Car car;
+
+    public Car getCar() {
+        return car;
+    }
+
+    public void setCar(Car car) {
+        this.car = car;
+    }
+}
